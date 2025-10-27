5f266dfba05f30890eb6763dd901d00a1265bd36
thezanrutar <zan@zanrutar.com>
2025-10-28 00:10:19 +0100

    new folders, files

---
+++ src/structures/competition.py
@@ -0,0 +1,4 @@
+class Competition:
+    def __init__(self, title, date):
+        self.title = title
+        self.date = date
---
+++ src/structures/team.py
@@ -0,0 +1,4 @@
+class Team:
+  def __init__(self, name, Category):
+      self.name = name
+      self.category = Category
