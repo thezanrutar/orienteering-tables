ed74b708393a794410a9a97da11946c4fb324961
thezanrutar <zan@zanrutar.com>
2025-11-10 12:14:39 +0100

    questios.java

---
+++ src/app/Questions.java
@@ -0,0 +1,14 @@
+package app;
+
+import competition.Team;
+
+public static class Questions {
+  private final char LETTER;
+
+  private String [] questionsA =
+
+  public Questions(Category category) {
+    LETTER = category.getLetter();
+  }
+
+}
--- src/competition/Team.java
+++ src/competition/Team.java
@@ -10,17 +10,16 @@ public class Team {
   private int startPoints;
   private int [] examValues = new Integer[2];
   private int [] drawingValues = new Integer[2];
-  private int [] livePoints =
-    new Integer[category.getLiveCheckpointN];
+  private int [] livePoints;
   private int checkpointsFound;
 
   private int membersN;
-  private int women;
-  private int children;
-  private int elderly;
+  private int womenN;
+  private int childrenN;
+  private int elderlyN;
 
-  private boolean firstAid;
-  private boolean shoes;
+  private boolean firstAid = true;
+  private int shoesN; // more negative => worse
   private boolean membership;
 
   private String [] members;
@@ -31,6 +30,7 @@ public class Team {
     this.name = name;
     this.category = category;
     category.addTeam(name, this);
+    livePoints = new Integer[category.getLiveCheckpointN];
   }
 
   // Competition
@@ -44,6 +44,39 @@ public class Team {
   }
 
   // Points
+  private void calculateStartPoints() {
+    int firstAidPoints = firstAid ? 0 : -15;
+    int membershipPoints = membership ? 0 : -10;
+    int shoesPoints;
+    if (shoesN >= 3) {
+      shoesPoints = -15;
+    } else if (shoesN == 2) {
+      shoesPoints = -10;
+    } else if (shoesN == 1) {
+      shoesPoints = -5;
+    } else { shoesPoints = 0; }
+    int womenPoints = womenN * 2;
+    int elderlyPoints = elderlyN * 2;
+    int childrenPoints;
+    if (childrenN >= 2) {
+      childrenPoints = 10;
+    } else if (childrenN == 1) {
+      childrenPoints = 5;
+    } else { childrenPoints = 0; }
+    int memberPoints;
+    switch (membersN) {
+      case 5:
+        memberPoints = 10;
+        break;
+      case 2:
+        memberPoints = -10;
+        break;
+      default:
+        memberPoints = 0;
+    }
+    startPoints = firstAidPoints + memberPoints + shoesPoints
+      + womenPoints + elderlyPoints + childrenPoints + memberPoints;
+  }
   private void calculatePoints() {
     int examPoints = examValues[0] * 5 - examValues[1] * 2;
     int drawingPoints = drawingValues[0] * 20 - drawingValues[1] * 5;
@@ -68,6 +101,7 @@ public class Team {
   public void addDrawingValues(int correct, int incorrect) {
     drawingValues[0] += correct;
     drawingValues[1] += incorrect;
+    calculatePoints();
   }
   public void addLivePoints(int i, int points) {
     livePoints[i] = points;
