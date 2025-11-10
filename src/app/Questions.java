f7b9f38fe462e9f25a3f18b4a6f40bfb1f9a390e
thezanrutar <zan@zanrutar.com>
2025-11-10 23:22:19 +0100

    questions

--- src/app/Questions.java
+++ src/app/Questions.java
@@ -5,7 +5,12 @@ import competition.Team;
 public static class Questions {
   private final char LETTER;
 
-  private String [] questionsA =
+  private String [] questionsA = {
+    "Team category:",
+    "Team name:",
+    "Team club:",
+    "Members:",
+
 
   public Questions(Category category) {
     LETTER = category.getLetter();
